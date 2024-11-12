a1e5f7f4c8a9a76c4ef0a05f7e3619aa06b6577f
NaufalArdian12 <naufalopo87@gmail.com>
2024-11-12 14:07:23 +0700

    pegawai model

---
+++ pegawai.java
@@ -0,0 +1,20 @@
+public class Pegawai {
+    public String nip;
+    public String nama;
+
+    // Default constructor
+    public Pegawai() {
+    }
+
+    // Constructor with parameters
+    public Pegawai(String nip, String nama) {
+        this.nip = nip;
+        this.nama = nama;
+    }
+
+    // Method to display employee information
+    public void displayInfo() {
+        System.out.println("NIP: " + nip);
+        System.out.println("Nama: " + nama);
+    }
+}
